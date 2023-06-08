36771244f5e0baced5c26e07c7f77a062f856805
Mike695 <78675581+Mike695@users.noreply.github.com>
2023-06-08 11:58:48 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -74,6 +74,21 @@ def nearest(df):
     mapdf={"lat":maplat,"lon":maplon}
     points=pd.DataFrame(data=mapdf)
     st.map(points)
+    
+API_KEY="918713575dff83f79a7ac9309a272f9c"
+def get_weather(lat,lon):
+    url= "https://api.openweathermap.org/data/2.5/weather?&lat="+lat+"&lon"+lon+"&appod="+API_KEY
+    response=requests.get(url)
+    response_json=response.json()
+    return response_json
+
+
+def kelvin_to_F(k):
+    return (k-273.15)*9/5+32
+
+def kelvin_to_c(k):
+    return k-273.15
+
 temp=get_weather('43.605366','3.881346')
 kelvin_to_F(temp)
    
