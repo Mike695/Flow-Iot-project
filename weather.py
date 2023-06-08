b844a841eda3e49b3a3eff5b5c58dc7c92f43728
Mike695 <78675581+Mike695@users.noreply.github.com>
2023-06-08 11:43:06 +0200

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+API_KEY="918713575dff83f79a7ac9309a272f9c"
+
+def get_weather(lat,lon):
+    url= "https://api.openweathermap.org/data/2.5/weather?&lat="+lat+"&lon"+lon+"&appod="+API_KEY
+    response=requests.get(url)
+    response_json=response.json
+    return response_json
+
+
+def kelvin_to_F(k):
+    return (k-273.15)*9/5+32
+
+def kelvin_to_c(k):
+    return k-273.15
